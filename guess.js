95884f8d17b1c3f121cf13a4f5787eeb4cd01b0a
apretado <jithorn+github@byu.edu>
2024-02-27 21:22:25 -0700

    Change all buttons colors when choice is made and display message to user to indicate if they selected the correct choice

--- guess.js
+++ guess.js
@@ -8,3 +8,36 @@ function buttonClick(button) {
         button.style.backgroundColor = "red";
     }
 }
+
+function guess(button) {
+    userGuessedCorrectly = false;
+
+    // Loop through every button
+    buttonList = document.querySelectorAll("button");
+    for (const item of buttonList) {
+        // Disable button
+        item.disabled = true;
+
+        // If correct
+        if (item.textContent === correctAnswer) {
+            item.style.backgroundColor = "#28a745";
+
+            // If this is the button the user clicked on
+            if (item === button) {
+                userGuessedCorrectly = true;
+            }
+        }
+
+        // If incorrect
+        else {
+            item.style.backgroundColor = "red";
+        }
+    }
+
+    if (userGuessedCorrectly) {
+        document.getElementById("options").innerHTML += "<p>Correct! +100 points</p>";
+    } else {
+        document.getElementById("options").innerHTML += "<p>Incorrect!</p>";
+
+    }
+}
