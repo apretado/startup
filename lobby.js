afbddff38b8beda0bdcb0f491c0d824e0d9d8c10
apretado <jithorn+github@byu.edu>
2024-02-27 20:18:30 -0700

    Make delay variable

--- lobby.js
+++ lobby.js
@@ -1,9 +1,9 @@
 document.getElementById("nameDisplay").textContent = "Your name: " + localStorage.getItem("username");
 
-setInterval(() => {
+for (let delay = 700; delay < 8000; delay += 2000) {
+    setTimeout(() => {
     const playerList = document.getElementById("playerList");
     const playerName = "player" + Math.floor(Math.random() * 1000);
-    playerList.innerHTML =
-      `<li>${playerName}</li>` +
-      playerList.innerHTML;
-  }, 5000);
+    playerList.innerHTML = `<li>${playerName}</li>` + playerList.innerHTML;
+    }, delay);
+}
