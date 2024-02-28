c1e71748b54daff8933cf985bbee67f584330dbc
apretado <jithorn+github@byu.edu>
2024-02-27 20:07:43 -0700

    Simulate players joining

--- lobby.js
+++ lobby.js
@@ -1,1 +1,9 @@
 document.getElementById("nameDisplay").textContent = "Your name: " + localStorage.getItem("username");
+
+setInterval(() => {
+    const playerList = document.getElementById("playerList");
+    const playerName = "player" + Math.floor(Math.random() * 1000);
+    playerList.innerHTML =
+      `<li>${playerName}</li>` +
+      playerList.innerHTML;
+  }, 5000);
