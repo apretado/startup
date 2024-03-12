1e0080c76a22b7f33cad4e1777e54563a670e770
apretado <jithorn+github@byu.edu>
2024-03-11 22:00:42 -0600

    Add call to Chuck Norris Jokes API

--- public/login.js
+++ public/login.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const jokeElement = document.createElement("p");
+  fetch("https://api.chucknorris.io/jokes/random")
+    .then(resonse => resonse.json())
+    .then(data => jokeElement.textContent = data.value);
+  document.querySelector("main").appendChild(jokeElement);
+});
+
 function login() {
     const nameEl = document.querySelector("#username");
     localStorage.setItem("username", nameEl.value);
