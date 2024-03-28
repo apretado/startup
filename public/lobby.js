fd33321a1bbc96d4789935a9834a6403385fb320
apretado <jithorn+github@byu.edu>
2024-03-27 22:02:43 -0600

    Create functions to add and remove players from the list

--- public/lobby.js
+++ public/lobby.js
@@ -1,9 +1,22 @@
 document.getElementById("nameDisplay").textContent = "Your name: " + localStorage.getItem("userName");
 
-for (let delay = 700; delay < 8000; delay += 2000) {
-    setTimeout(() => {
+// for (let delay = 700; delay < 8000; delay += 2000) {
+//     setTimeout(() => {
+//     const playerList = document.getElementById("playerList");
+//     const playerName = "player" + Math.floor(Math.random() * 1000);
+//     playerList.innerHTML = `<li>${playerName}</li>` + playerList.innerHTML;
+//     }, delay);
+// }
+
+function appendPlayer(name) {
     const playerList = document.getElementById("playerList");
-    const playerName = "player" + Math.floor(Math.random() * 1000);
-    playerList.innerHTML = `<li>${playerName}</li>` + playerList.innerHTML;
-    }, delay);
+    const li = document.createElement('li');
+    li.textContent = escapeHTML(name);
+    li.setAttribute('name', escapeHTML(name));
+    playerList.appendChild(li);
+}
+
+function removePlayer(name) {
+    const toRemove = document.querySelector(`#playerList li[name="${escapeHTML(name)}"]`);
+    toRemove.parentNode.removeChild(toRemove);
 }
