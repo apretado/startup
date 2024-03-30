30c136cf3f4c77391622e70fb7aaf1ff0dbabf05
apretado <jithorn+github@byu.edu>
2024-03-29 19:39:07 -0600

    Add front-end websocket logic

--- public/lobby.js
+++ public/lobby.js
@@ -1,3 +1,5 @@
+let socket;
+
 document.getElementById("nameDisplay").textContent = "Your name: " + localStorage.getItem("userName");
 
 // for (let delay = 700; delay < 8000; delay += 2000) {
@@ -8,15 +10,43 @@ document.getElementById("nameDisplay").textContent = "Your name: " + localStorag
 //     }, delay);
 // }
 
+
+// 100% vulnerable to XSS
 function appendPlayer(name) {
     const playerList = document.getElementById("playerList");
     const li = document.createElement('li');
-    li.textContent = escapeHTML(name);
-    li.setAttribute('name', escapeHTML(name));
+    li.textContent = name;
+    li.setAttribute('name', name);
     playerList.appendChild(li);
 }
 
 function removePlayer(name) {
-    const toRemove = document.querySelector(`#playerList li[name="${escapeHTML(name)}"]`);
+    const toRemove = document.querySelector(`#playerList li[name="${name}"]`);
     toRemove.parentNode.removeChild(toRemove);
 }
+
+// Open webSocket
+function configureWebSocket() {
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    console.log("WebSocket configured");
+
+    socket.onopen = (event) => { 
+        // Let the server know that you have joined the lobby (todo: authentication)
+        socket.send(JSON.stringify({ type: "lobbyJoin", name: localStorage.getItem("userName") }));
+        console.log("Name sent");
+    };
+
+    // Do something when WebSocket closes
+    // socket.onclose = (event) => {
+    // };
+
+    socket.onmessage = async (event) => {
+        const msg = JSON.parse(await event.data.text());
+        // Do something depending on what type of message it is
+        if (msg.type === "lobbyJoin") {
+            appendPlayer(msg.name);
+        }
+    };
+}
+configureWebSocket();
