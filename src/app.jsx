6db3442d38b361a30f80de945bca1adb46efbb0f
apretado <jithorn+github@byu.edu>
2024-04-11 20:16:14 -0600

    Create router

--- src/app.jsx
+++ src/app.jsx
@@ -1,42 +1,37 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from "./login/login";
+import { Play } from "./play/play";
 import './app.css';
 
 export default function App() {
   return (
-    <div className='body'>
-        <header>
-            <nav>
-            <menu>
-                <h1>I know you</h1>
-            </menu>
-            </nav>
-        </header>
-        <main>
-            App components go here
-            {/* <div id="loginControls" style="display: none">
-                <h2>Sign in</h2>
-                <label for="username">Username</label>
-                <br />
-                <input type="text" id="userName" placeholder="Username" />
-                <br />
-                <label for="name">Password</label>
-                <br />
-                <input type="password" id="userPassword" placeholder="Password" />
-                <br />
-                <button onclick="loginUser()">Login</button>
-                <button onclick="createUser()">Create</button>
-            </div>
-            <div id="playControls" style="display: none">
-                <div id="playerName"></div>
-                <button onclick="play()">Play</button>
-                <button onclick="logout()">Logout</button>
-            </div> */}
-        </main>
-        <footer>
-            <p>Made by Jonathan Thornton</p>
-            <a href="https://github.com/apretado/startup">GitHub</a>
-        </footer>
-        <script src="login.js"></script>
-    </div>
+    <BrowserRouter>
+        <div className='body'>
+            <header>
+                <nav>
+                    <menu>
+                        <h1>I know you</h1>
+                    </menu>
+                </nav>
+            </header>
+            
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/play' element={<Play />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+
+            <footer>
+                <p>Made by Jonathan Thornton</p>
+                <a href="https://github.com/apretado/startup">GitHub</a>
+            </footer>
+            <script src="login.js"></script>
+        </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main>404 Page Not Found</main>;
 }
---
+++ src/login/login.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export function Login() {
+  return (
+    <main>
+      <div>login displayed here</div>
+    </main>
+  );
+}
+
+// <div id="loginControls" style="display: none">
+//     <h2>Sign in</h2>
+//     <label for="username">Username</label>
+//     <br />
+//     <input type="text" id="userName" placeholder="Username" />
+//     <br />
+//     <label for="name">Password</label>
+//     <br />
+//     <input type="password" id="userPassword" placeholder="Password" />
+//     <br />
+//     <button onclick="loginUser()">Login</button>
+//     <button onclick="createUser()">Create</button>
+// </div>
+// <div id="playControls" style="display: none">
+//     <div id="playerName"></div>
+//     <button onclick="play()">Play</button>
+//     <button onclick="logout()">Logout</button>
+// </div>
